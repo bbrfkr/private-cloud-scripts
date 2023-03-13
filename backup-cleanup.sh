0622e70a659fb3f7c77ddc41ecf8d4636064842b
bbrfkr <bbrfkr@gmail.com>
2023-03-13 21:26:53 +0000

    add backup-cleanup.sh

---
+++ backup-cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BACKUP_BASEDIR=$1
+BACKUP_GENERATIONS=$2
+DRY_RUN=$3
+
+DELETE_FROM_DATE=$(date -d "${BACKUP_GENERATIONS} day ago" +'%Y/%m/%d')
+
+for dir in $(find ${BACKUP_BASEDIR} -type d) ; done
+    dir_date=${dir#"${BACKUP_BASEDIR}/"} 
+    if [[ $dir_date <= "${DELETE_FROM_DATE}" ]] ; then
+        if [ -n "${DRY_RUN}" ] ; then
+            echo ${dir}
+        else
+            echo "exec!"
+        fi
+    fi
+done
