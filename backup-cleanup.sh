b0347d7b7cfa4c40a85dc6f349532ddd15228f2f
bbrfkr <bbrfkr@gmail.com>
2023-03-13 21:52:47 +0000

    bugfix

--- backup-cleanup.sh
+++ backup-cleanup.sh
@@ -7,7 +7,7 @@ DRY_RUN=$3
 DELETE_FROM_DATE=$(date -d "${BACKUP_GENERATIONS} day ago" +'%Y-%m-%d')
 
 for file in $(find ${BACKUP_DIR} -type f) ; do
-    file_date=${dir%"_*.sql"} 
+    file_date=$(echo -n ${file} | awk -F '[_]' '{print $1}')
     if [[ ! "${file_date}" > "${DELETE_FROM_DATE}" ]] ; then
         if [ -n "${DRY_RUN}" ] ; then
             echo ${file}
