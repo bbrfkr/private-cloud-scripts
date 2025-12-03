508a41702ec789fc5816d5c14e72ce12090b6f49
bbrfkr <bbrfkr@gmail.com>
2025-12-03 16:53:25 +0900

    change backup procedures

--- backup-cleanup.sh
+++ backup-cleanup.sh
@@ -6,13 +6,13 @@ DRY_RUN=$3
 
 DELETE_FROM_DATE=$(date -d "${BACKUP_GENERATIONS} day ago" +'%Y-%m-%d')
 
-for file in $(find ${BACKUP_DIR} -type f) ; do
-    file_date=$(echo -n ${file} | awk -F"${BACKUP_DIR}/" '{print $2}' | awk -F_ '{print $1}')
-    if [[ ! "${file_date}" > "${DELETE_FROM_DATE}" ]] ; then
+for directory in $(find ${BACKUP_DIR} -type d) ; do
+    dir_date=$(echo -n ${directory} | awk -F"${BACKUP_DIR}/" '{print $2}' | awk -F_ '{print $1}')
+    if [[ ! "${dir_date}" > "${DELETE_FROM_DATE}" ]] ; then
         if [ -n "${DRY_RUN}" ] ; then
-            echo ${file}
+            echo ${directory}
         else
-            rm -f ${file}
+            rm -rf ${directory}
         fi
     fi
 done
--- database-backup.sh
+++ database-backup.sh
@@ -6,7 +6,7 @@ NOTIFICATION_URL=$3
 DEBUG=$4
 DATE=$(date '+%Y-%m-%d')
 
-(df | grep /share/backups > /dev/null) && mysqldump --databases ${TARGET_DATABASE} --flush-logs --single-transaction > ${BACKUP_DIR}/${DATE}_${TARGET_DATABASE}.sql
+(df | grep /share/backups > /dev/null) && mysqldump --databases ${TARGET_DATABASE} --flush-logs --single-transaction > ${BACKUP_DIR}/${TARGET_DATABASE}.sql
 
 if [ $? -ne 0 ] ; then
     curl -XPOST -H'Content-Type:application/json' -d "{\"content\":\"failed to backup for database ${TARGET_DATABASE}!!\"}" ${NOTIFICATION_URL}
