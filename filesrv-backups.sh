d5e989109563bb18415845811726fcc952406ebc
bbrfkr <bbrfkr@gmail.com>
2025-12-03 17:19:52 +0900

    change backup procedures

--- filesrv-backups.sh
+++ filesrv-backups.sh
@@ -2,4 +2,4 @@
 TARGET_DIR=$1
 BACKUP_DIR=$2
 mkdir -p $BACKUP_DIR
-rsync -avh --delete --backup --backup-dir="../backups_old/backup_$(date +%Y-%m-%d)" $TARGET_DIR $BACKUP_DIR/backup_latest
+rsync -avh --delete --backup --backup-dir="../old/$(date +%Y-%m-%d)" $TARGET_DIR $BACKUP_DIR/latest
